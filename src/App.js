e6293a443afeb2f90acb41b8b78c8b7ce014171b
Mike <mskowron4@yahoo.com>
2022-12-06 13:51:35 -0800

    setting up App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+function App() {
+    return(
+<div>
+    <Nav/>
+    <Header/>
+    <Project/>
+    <Footer/>   
+</div>
+    )
+}
+
+export default App;
